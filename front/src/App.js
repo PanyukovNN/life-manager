5de15d529a90e0a8e81012da90b99512871c1f05
Nickolay <zylex77@gmail.com>
2021-11-30 23:41:11 +0300

    creating index page

--- front/src/App.js
+++ front/src/App.js
@@ -2,24 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="filter-block">
+                <div className="filter-block-header">
+                    Список задач
+                </div>
+
+                <div className="selector-block">
+                    <div className="selector-header">
+                        Раздел:
+                    </div>
+                    <div className="selector-wrapper">
+                        <select size="1">
+                            <option>Работа</option>
+                            <option>Домашние дела</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="selector-block">
+                    <div className="selector-header">
+                        За период:
+                    </div>
+                    <div className="selector-wrapper">
+                        <select size="1">
+                            <option>День</option>
+                            <option>Неделя</option>
+                            <option>Месяц</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div className="tasks-block">
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
