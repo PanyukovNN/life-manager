46efd425374227346e299f8c3929eddec3aa58b1
Nickolay <zylex77@gmail.com>
2021-12-14 19:59:30 +0300

    fix exception handling

--- back/src/main/java/org/panyukovnn/lifemanager/controller/RestResponseEntityExceptionHandler.java
+++ back/src/main/java/org/panyukovnn/lifemanager/controller/RestResponseEntityExceptionHandler.java
@@ -7,14 +7,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import static org.panyukovnn.lifemanager.model.Constants.ERROR_OCCURRED_MSG;
 
@@ -22,23 +18,23 @@ import static org.panyukovnn.lifemanager.model.Constants.ERROR_OCCURRED_MSG;
  * Адвайс для обработки исключений контроллеров
  */
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler {
 
     /**
      * Обработать исключение
+     *
      * @param e исключение
      * @return тело ответа о возникшем исключении
      */
-    @ExceptionHandler(value = { Exception.class })
+    @ExceptionHandler(value = {Exception.class})
     protected ResponseEntity<Object> handleConflict(Exception e) {
         e.printStackTrace();
 
         return ResponseEntity.internalServerError().body(ERROR_OCCURRED_MSG + e.getMessage());
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
-                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
         e.printStackTrace();
 
         Map<String, String> errors = new HashMap<>();
@@ -48,6 +44,6 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
             errors.put(fieldName, errorMessage);
         });
 
-        return new ResponseEntity<>(errors, status);
+        return ResponseEntity.badRequest().body(errors);
     }
 }
